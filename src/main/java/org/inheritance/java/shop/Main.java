15c9f4544cf5df129481438caeed84de2894baaa
giogiosgr <giovannisgr88@gmail.com>
2024-06-19 12:05:44 +0200

    feat: utilizzo della classe Smartphone

--- src/main/java/org/inheritance/java/shop/Main.java
+++ src/main/java/org/inheritance/java/shop/Main.java
@@ -5,8 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 	    //creazione e utilizzo istanza della classe Smartphone
-		Smartphone phone = new Smartphone ("Azuz", "Nuovissimo", 730, 20, "0985-1430-0001-0002", (short) 4);
+		Smartphone phone = new Smartphone ("Azuz", "Nuovissimo", 699.99, 20, "0985-1430-0001-0002", (short) 4);
 		System.out.println("Nome completo dello smartphone: " + phone.getFullName());
+		System.out.println("Prezzo completo dello smartphone: " + phone.getFullPrice());
 		phone.setMemory((short) 8);
 		System.out.printf("Memoria dello smartphone: %d GB \n", phone.getMemory());
 		System.out.println("Codice IMEI dello smartphone: " + phone.getIMEI());
