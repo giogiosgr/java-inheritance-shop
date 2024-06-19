2ec905221b1398ca0af690c35e7ec35e19d3469f
giogiosgr <giovannisgr88@gmail.com>
2024-06-19 13:19:41 +0200

    feat: classe Televisori che estende Prodotto

--- src/main/java/org/inheritance/java/shop/Televisori.java
+++ src/main/java/org/inheritance/java/shop/Televisori.java
@@ -1,10 +1,28 @@
 package org.inheritance.java.shop;
 
 public class Televisori extends Prodotto {
+	private short inches;
+	private boolean isSmart;
 
-	public Televisori(String name, String description, double price, int tax) {
+	public Televisori(String name, String description, double price, int tax, short inches, boolean isSmart) {
 		super(name, description, price, tax);
-
+		this.inches = inches;
 	}
 
+	// getters e setters
+	short getInches() {
+		return this.inches;
+	}
+
+	boolean isSmart() {
+		return this.isSmart;
+	}
+
+	void setInches(short newInches) {
+		this.inches = newInches;
+	}
+
+	void setIsSmart(boolean smart) {
+		this.isSmart = smart;
+	}
 }
