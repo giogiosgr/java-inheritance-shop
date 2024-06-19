e718fc0f463ab565bcb31d3aeac0774cdeec5659
giogiosgr <giovannisgr88@gmail.com>
2024-06-19 11:55:22 +0200

    feat: classe Smartphone che estende Prodotto

--- src/main/java/org/inheritance/java/shop/Smartphone.java
+++ src/main/java/org/inheritance/java/shop/Smartphone.java
@@ -1,10 +1,26 @@
 package org.inheritance.java.shop;
 
-public class Smartphone extends Prodotto{
+class Smartphone extends Prodotto {
+	//codice IMEI dichiarato come costante, mentre la memoria sarà modificabile
+    final private String IMEI;
+	private short memory;
 
-	public Smartphone(String name, String description, double price, int tax) {
+	Smartphone(String name, String description, double price, int tax, String IMEI, short memory) {
 		super(name, description, price, tax);
-		
+		this.IMEI = IMEI;
+		this.memory = memory;
 	}
 
+    //getters e setters
+    String getIMEI() {
+		return this.IMEI;
+	}
+
+	short getMemory () {
+		return this.memory;
+	}
+
+	void setMemory(short newMemory) {
+		this.memory = newMemory;
+	}
 }
