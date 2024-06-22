7d30f6f67e84a881c506fb41658659591fccc66c
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-22 18:18:27 +0200

    feat: ciclo di input

--- src/main/java/org/inheritance/java/shop/Carrello.java
+++ src/main/java/org/inheritance/java/shop/Carrello.java
@@ -1,23 +1,88 @@
 package org.inheritance.java.shop;
 
+import java.util.Scanner;
+
 class Carrello {
 	
 	public static void main(String[] args) {
 		
+        Scanner sc = new Scanner(System.in);
+        //array dei prodotti
+	    Prodotto[] prodotti = new Prodotto[0];
+	    //variabili di appoggio per inizializzare le classi
+	    String product;
+	    String name;
+	    String description;
+	    double price;
+	    double tax = 20;
+	    Short memory;
+	    Short inches;
+	    String color;
+	    boolean flag;
+	    
+	    /**
+	     * Ciclo per lo scan delle varie proprietà e creazione degli oggetti di tipo Prodotto,
+	     * finisce quando l'utente sceglie di uscire, nell'input posto nella condizione del while,
+	     * L'unico controllo dell'input avviene nella scelta del tipo prodotto
+	     */
+        do {
+        	do {
+        		System.out.println("\nScegli il tipo di prodotto (smartphone, televisore, cuffie)");
+        	    product = sc.next().toLowerCase();
+        	} while (!product.equals("smartphone") && !product.equals("televisore") && !product.equals("cuffie"));
+            System.out.printf("\nInserire nome %s: \n", product);
+            name = sc.next();
+            System.out.printf("\nInserire descrizione %s: \n", product);
+            description = sc.next();
+            System.out.printf("\nInserire prezzo %s: \n", product);
+            price = sc.nextDouble();
+            switch (product) {
+                case "smartphone":
+                	System.out.println("\nInserire la memoria in GB dello smartphone");
+                	memory = sc.nextShort();
+                	prodotti = addProduct(prodotti, new Smartphone(name, description, price, tax, memory));
+                	break;
+                case "televisore":
+                	System.out.println("\nInserire la dimensione in pollici del televisore:");
+                	inches = sc.nextShort();
+                	System.out.println("\nScegliere se il televisore è Smart (s/n)");
+                	if (sc.next().toLowerCase().equals("s")) {
+                		flag = true;
+                	} else {
+                		flag = false;
+                	}
+                	prodotti = addProduct(prodotti, new Televisori(name, description, price, tax, inches, flag));
+                	break;	
+                case "cuffie":
+                	System.out.println("\nInserire il colore delle cuffie");
+                	color = sc.next();
+                	System.out.println("\nScegliere se le cuffie sono Wireless (s/n)");
+                	if (sc.next().toLowerCase().equals("s")) {
+                		flag = true;
+                	} else {
+                		flag = false;
+                	}
+                	prodotti = addProduct(prodotti, new Cuffie(name, description, price, tax, color, flag));
+            }
+            System.out.println("\nVuoi inserire un altro prodotto?");      	
+        } while (sc.next().toLowerCase().equals("s"));  	    
+        
+        sc.close();
 	
-	Prodotto[] prodotti = new Prodotto[0];
-		
-	prodotti = addProduct(prodotti, new Televisori("Ranasonic", "Top di gamma", 3499, 20, (short) 75, false));
-	
-	
-	for (int i = 0; i < prodotti.length; i++) {
-		System.out.println(prodotti[i].toString());
-		
-	}
-    
-		
-		
-	
+	    /* sotto commento, prodotti istanziati manualmente per test
+	    prodotti = addProduct(prodotti, new Televisori("Ranasonic", "Top di gamma", 3499, 20, (short) 75, true));
+	    prodotti = addProduct(prodotti, new Smartphone("Azuz", "Nuovissimo", 699.99, 20, (short) 4));
+	    prodotti = addProduct(prodotti, new Cuffie("Phony", "Bassi potenti", 180.50, 15, "Viola", false));
+	    */
+	    
+        System.out.println("\n---- CARRELLO PRODOTTI ----\n");
+        /**
+         * Ciclo per la stampa dell'array dei prodotti, 
+         * richiamando l'Override del metodo toString() dell'oggetto
+         */
+	    for (int i = 0; i < prodotti.length; i++) {
+	    	System.out.printf("%d° prodotto del carrello: %s\n\n", i+1, prodotti[i].toString());
+    	}
 	}
 	
 	/**
