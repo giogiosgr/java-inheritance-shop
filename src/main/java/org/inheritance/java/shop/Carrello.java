da8cae6cfc104cc48e297115b4f2e54bf2463503
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-22 12:25:31 +0200

    feat: metodo aggiungiProdotto

--- src/main/java/org/inheritance/java/shop/Carrello.java
+++ src/main/java/org/inheritance/java/shop/Carrello.java
@@ -1,10 +1,39 @@
 package org.inheritance.java.shop;
 
-public class Carrello {
-
+class Carrello {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	
+	Prodotto[] prodotti = new Prodotto[0];
+		
+	prodotti = addProduct(prodotti, new Televisori("Ranasonic", "Top di gamma", 3499, 20, (short) 75, false));
+	
+	
+	for (int i = 0; i < prodotti.length; i++) {
+		System.out.println(prodotti[i].toString());
+		
 	}
-
+    
+		
+		
+	
+	}
+	
+	/**
+	 * Metodo per aggiungere un nuovo prodotto, con restituzione di un nuovo array di lunghezza n+1
+	 * 
+	 * @param oldProdotti
+	 * @param newProdotto
+	 * @return
+	 */
+	static Prodotto[] addProduct(Prodotto[] oldProdotti, Prodotto newProdotto) {
+		Prodotto[] newProdotti = new Prodotto[oldProdotti.length + 1];
+		for (int i = 0; i < newProdotti.length - 1; i++) {
+			newProdotti[i] = oldProdotti[i];
+		}
+		newProdotti[oldProdotti.length] = newProdotto;
+		return newProdotti;	
+	}
+	
 }
